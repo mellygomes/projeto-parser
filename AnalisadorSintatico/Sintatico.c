5a5fbadaa1845efe3cee2d917806c7bc324c218c
Emanuelly <emanuellygomes.manu2004@gmail.com>
2024-10-30 00:55:03 -0300

    Analise sintática da indentificação do programa e das declarações de var

--- AnalisadorSintatico/Sintatico.c
+++ AnalisadorSintatico/Sintatico.c
@@ -49,6 +49,7 @@ void VarDeclarations(AnalisadorLexico *lex, char *tbl[], No *TS) {
 }
 
 void VarDeclaration(AnalisadorLexico *lex, char *tbl[], No *TS, Token token) {
+
     if (strcmp(token.nome, "ID") != 0) {
         printf("ERRO: You must do var declaration (linha: %i)", token.linha);
         exit(EXIT_FAILURE);
@@ -91,7 +92,8 @@ void CodeBlock(AnalisadorLexico *lex, char *tbl[], No *TS) {
     IniciarToken(&token);
     token = getToken(lex, tbl, TS);
 
-    while (strcmp(token.nome, "ID") == 0) {
+    //antes de iniciar analise do bloco de codigo, verifica se uma nova lista de declarações não iniciou 
+    while (strcmp(token.nome, "ID") == 0) { // se sim, VarDeclaration é chamada para analisar
         VarDeclaration(lex, tbl, TS, token);
         token = getToken(lex, tbl, TS);
     }
@@ -100,7 +102,66 @@ void CodeBlock(AnalisadorLexico *lex, char *tbl[], No *TS) {
         printf("ERRO: command section expected (linha: %i)", token.linha); 
         exit(EXIT_FAILURE);
     } else {
-        return;
+        // CommandLine(lex, tbl, TS);
+        token = getToken(lex, tbl, TS);
+        if (strcmp(token.valor, "end") != 0) {
+            printf("ERRO: command section not finished correctly (linha: %i, coluna: %i)", token.linha, token.coluna); 
+            exit(EXIT_FAILURE);   
+        }
     }
 }
- 
+
+void CommandLine(AnalisadorLexico *lex, char *tbl[], No *TS) {
+    Token token;
+    IniciarToken(&token);
+    token = getToken(lex, tbl, TS);
+    
+    if (strcmp(token.nome, "ID") == 0) {
+        Atribuition(lex, tbl, TS);
+    } else if (strcmp(token.valor, "begin") == 0) {
+        //outro bloco
+    } else if (strcmp(token.valor, "if") == 0) {
+        //comando condicional
+    } else if (strcmp(token.valor, "while") == 0) {
+        //comando de repeticao
+    } else {
+        printf("ERRO: command section expected (linha: %i, coluna: %i)", token.linha, token.coluna); 
+        exit(EXIT_FAILURE);  
+    }
+}
+
+void Atribuition(AnalisadorLexico *lex, char *tbl[], No *TS) {
+    Token token;
+    IniciarToken(&token);
+    token = getToken(lex, tbl, TS);
+
+    if (strcmp(token.valor, ":=") != 0) {
+        printf("ERRO: Expected token ':=' in (linha: %i, coluna: %i)", token.linha, token.coluna);
+        exit(EXIT_FAILURE);    
+    } else {
+        // Expression(lex, tbl, TS);
+    }      
+}
+
+// void Expression(AnalisadorLexico *lex, char *tbl[], No *TS) {
+//     SimpleExpression(lex, tbl, TS);
+// }
+
+// void SimpleExpression(AnalisadorLexico *lex, char *tbl[], No *TS) {
+//     Token token;
+//     IniciarToken(&token);
+//     token = getToken(lex, tbl, TS);
+
+//     if ((strcmp(token.valor, "+") ==  0) || (strcmp(token.valor, "-") ==  0)) {
+//         token = getToken(lex, tbl, TS);
+//     }
+
+//     Factor(lex, tbl, TS, token);
+//     if ((strcmp(token.valor, "+") !=  0) && (strcmp(token.valor, "-") !=  0)) {
+//         printf("ERRO: Invalid syntax for expression (linha: %i, coluna: %i)", token.linha, token.coluna);
+//         exit(EXIT_FAILURE);
+//     }
+
+//     token = getToken(lex, tbl, TS);
+//     Factor(lex, tbl, TS, token);
+// }
--- AnalisadorSintatico/Sintatico.h
+++ AnalisadorSintatico/Sintatico.h
@@ -10,5 +10,12 @@ void AnaliseProgram(AnalisadorLexico *lex, char *tbl[], No *TS);
 void VarDeclarations(AnalisadorLexico *lex, char *tbl[], No *TS);
 void VarDeclaration(AnalisadorLexico *lex, char *tbl[], No *TS, Token token);
 void CodeBlock(AnalisadorLexico *lex, char *tbl[], No *TS);
+void CommandLine(AnalisadorLexico *lex, char *tbl[], No *TS);
+void Atribuition(AnalisadorLexico *lex, char *tbl[], No *TS);
+// void Conditional(AnalisadorLexico *lex, char *tbl[], No *TS);
+// void Repetitive(AnalisadorLexico *lex, char *tbl[], No *TS);
+// void Expression(AnalisadorLexico *lex, char *tbl[], No *TS);
+// void SimpleExpression(AnalisadorLexico *lex, char *tbl[], No *TS);
+// void Factor(AnalisadorLexico *lex, char *tbl[], No *TS, Token token);
 
 #endif
